0208dc9b51e94ad19872bd31bf53e641f327a6e2
val <valentin.vivies@gmail.com>
2016-08-08 14:54:37 +0200

    Ajout de la facade TMDB

--- config/app.php
+++ config/app.php
@@ -202,6 +202,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'TMDB' => Tmdb\Laravel\Facades\Tmdb::class,
 
     ],
 
