b0a3a1eb71b4517d17e4bbbd6e29191dc2f655a7
val <valentin.vivies@gmail.com>
2016-08-15 21:24:16 +0200

    Mise en place du slug : pour les routes

--- app/Video.php
+++ app/Video.php
@@ -45,4 +45,15 @@ class Video extends Model
             ]
         ];
     }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
 }
